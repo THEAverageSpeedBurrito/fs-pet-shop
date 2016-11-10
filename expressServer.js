f0ee0402b2b5ae4fc39b2fa8dca94d33622fd4ff
Evan Busse <evanbusse@gmail.com>
2016-11-10 10:47:18 -0700

    Completed Express server logic

--- expressServer.js
+++ expressServer.js
@@ -7,19 +7,38 @@ var port = process.env.PORT || 3000;
 var fs = require('fs');
 var path = require('path');
 
+//get requests
+
 app.get('/pets/:id', function(req, res) {
   fs.readFile('./pets.json', 'utf8', function(err, petsJSON){
     if(err) {
       console.error(err.stack);
-      return res.sendStatud(500);
+      return res.sendStatus(500);
     }
 
     var id = Number.parseInt(req.params.id);
     var pets = JSON.parse(petsJSON);
 
-    res.send(pets[id]);
+    if(0 <= id && id < pets.length){
+      res.send(pets[id]);
+    }
+
+    res.sendStatus(404);
+
   });
 });
+
+app.get('/pets', function(req, res) {
+  fs.readFile('./pets.json', 'utf8', function(err, petsJSON){
+    if(err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    res.send(JSON.parse(petsJSON));
+  });
+});
+
 
 app.use(function(req, res) {
   res.sendStatus(404);
